0b58b92a4979f3dab3cc6c68b5f905663eacabd7
minho kim <54491294+kimue24@users.noreply.github.com>
2021-03-16 10:01:39 +0900

    20210316

--- study/src/main/java/kr/co/dao/MemberDAO.java
+++ study/src/main/java/kr/co/dao/MemberDAO.java
@@ -2,12 +2,15 @@ package kr.co.dao;
 
 import kr.co.vo.MemberVO;
 
-public class MemberDAO {
+public interface MemberDAO {
 	
-	public interface MemeberDAO {
-		
-		//회원가입
-		public void register(MemberVO vo) throws Exception;
-	}
-
+	// 회원가입
+	public void register(MemberVO vo) throws Exception;
+	
+	// 로그인
+	public MemberVO login(MemberVO vo) throws Exception;
+	
+	// 회원정보 수정
+	public void memberUpdate(MemberVO vo)throws Exception;
+	
 }
--- study/src/main/java/kr/co/dao/MemberDAOImpl.java
+++ study/src/main/java/kr/co/dao/MemberDAOImpl.java
@@ -1,8 +1,20 @@
-
-
 package kr.co.dao;
 
-public class MemberDAOImpl {
+import javax.inject.Inject;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import kr.co.vo.MemberVO;
+
+@Repository
+public class MemberDAOImpl implements MemberDAO {
+	
+	@Inject SqlSession sql;
+	// 회원가입
+
+	@Override
+	public void register(MemberVO vo) throws Exception {
+		sql.insert("memberMapper.register", vo);
+	}
 }
- 
